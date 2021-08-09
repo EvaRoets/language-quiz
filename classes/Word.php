5ed79987f910a9324b676d95ceebcdf62ef84efe
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-09 11:35:18 +0200

    Add answer verification WIP

--- classes/Word.php
+++ classes/Word.php
@@ -7,6 +7,20 @@ class Word
     public function verify(string $answer)
     {
         // TODO: use this function to verify if the provided answer by the user matches the correct one
+
+        $totalCorrect = 0;
+
+        if ("woord1 uit array " == strtolower($_POST("translation"))
+            {
+                $totalCorrect++;
+            }
+        if ("woord2 uit array " == strtolower($_POST("translation"))
+            {
+                $totalCorrect++;
+            }
+        echo "<div id='results'>$totalCorrect</div>";
+
+
         // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
         // Bonus (hard): can you allow answers with small typo's (max one character different)?
     }
