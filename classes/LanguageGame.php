727cf423b8a96764c747fffdb57e61374558e8bb
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-10 14:18:10 +0200

    Store option A and B for readability

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -19,25 +19,36 @@ class LanguageGame
 
     public function run()
     {
-        // Option A: select a random word for the user to translate
+
+
+    }
+
+    //option A: select a random word for the user to translate
+    public function generateWord()
+    {
         $this->randomWord = $this->words[array_rand($this->words, 1)];
-//        var_dump($this->words);
+        // var_dump($this->words);
+        // store randomWord in session
         $_SESSION["randomWord"] = $this->randomWord;
+    }
 
-        // Option B: user has just submitted an answer
+    //option B: user has just submitted an answer
+    public function processTranslation()
+    {
+        //call randomWord from session
+        $this->randomWord = $_SESSION["randomWord"];
+
         // verify the answer
-        // generate a message for the user that can be shown
-
         $playerTranslation = $_POST("translation");
 
+        // generate a message for the user that can be shown
         //if verify() is true
         if ($this->randomWord->verify($playerTranslation) === true) {
             $this->message = "<div>Tres bien, your answer <b>{$playerTranslation}</b> is correct. </b></b>Your score is" . $totalScore . "</div>";
             //TODO add score
-        } else { // if verify is false (words don't match)
+        } else {
             $this->message = "<div>!Dommage, your answer <b>{$playerTranslation}</b> is wrong. </b></b>Your score is" . $totalScore . "</div>";
             //TODO add score
         }
-
     }
 }
