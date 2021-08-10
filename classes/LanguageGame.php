cc893236ad7d720876abb11ad4cc406d30756486
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-10 15:10:08 +0200

    Refactor to fix errors

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -28,17 +28,14 @@ class LanguageGame
     }
 
     //option A: select a random word for the user to translate
-    public function generateWord()
+    private function generateWord()
     {
         $this->randomWord = $this->words[array_rand($this->words, 1)];
-        // var_dump($this->words);
-        // store randomWord in session
         $_SESSION["playerTranslation"] = serialize($this->randomWord);
-        var_dump($_SESSION["playerTranslation"]);
     }
 
     //option B: user has just submitted an answer
-    public function checkTranslation()
+    private function checkTranslation()
     {
         //call randomWord from session
         $this->randomWord = unserialize($_SESSION["playerTranslation"]);
@@ -49,10 +46,10 @@ class LanguageGame
         // generate a message for the user that can be shown
         //if verify() is true
         if ($this->randomWord->verify($playerTranslation) === true) {
-            $this->message = "<div>Tres bien, your answer <b>" . $playerTranslation . "</b> is correct. </b></b>Your score is" . $totalScore . "</div>";
+            $this->message = "<div>Tres bien, your answer <b>" . $playerTranslation . "</b> is correct. </b></div>";
             //TODO add score
         } else {
-            $this->message = "<div>!Dommage, your answer <b>" . $playerTranslation . "</b> is wrong. </b></b>Your score is" . $totalScore . "</div>";
+            $this->message = "<div>!Dommage, your answer <b>" . $playerTranslation . "</b> is wrong. </b></div>";
             //TODO add score
         }
     }
